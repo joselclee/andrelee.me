8c28686313987c25d29885c35314d54d1001d2c7
LevelAndre <andrelee089@gmail.com>
2023-11-27 05:18:21 -0800

    item3

--- webpage/src/components/Item3.js
+++ webpage/src/components/Item3.js
@@ -4,20 +4,18 @@ import Button from 'react-bootstrap/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCode } from '@fortawesome/free-solid-svg-icons'
 
-import snippetImage from './assets/numbers.png';
+import snippetImage from './assets/snippet1.jpg';
 
-const Item2 = () => {
+const Item3 = () => {
   return (
     <div>
         <Card>
         <Card.Img variant="top" src={snippetImage} alt="Snippet" />
             <Card.Body>
-                <Card.Title>Digit Classifier Model</Card.Title>
+                <Card.Title></Card.Title>
                 <Card.Text>
-                  This is a simple machine learning model that is able to
-                  recognize handwritten digits and classify them. The dataset
-                  is taken from MNIST and I have provided 18 hand-drawn 
-                  images to classify. The current accuracy of the model is 50%.
+                  This is a snippet of the code behind this website.
+                  Click the button below to view the repository.
                 </Card.Text>
                 <Button variant="secondary">
                   <FontAwesomeIcon icon={faCode} /></Button>
@@ -27,4 +25,4 @@ const Item2 = () => {
   )
 }
 
-export default Item2
+export default Item3
