4ed3adba11d6020340e97ae97a772526c1af8d15
LevelAndre <andrelee089@gmail.com>
2023-12-01 16:24:02 -0800

    link changes?
    Removed '#' from link references in header.js but not sure what they do- will need to read documentation for it.

--- webpage/src/components/Header.js
+++ webpage/src/components/Header.js
@@ -5,29 +5,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
 const Header = () => {
-  return (
-    // <div class="top">
-    //     <Navbar sticky="top" bg="secondary" data-bs-theme="dark">
-    //       <Container>
-    //       <Navbar.Brand href="top">andrelee.dev</Navbar.Brand>
-    //         </Container>
-    //     </Navbar>
-    //     <p>
-    //     I'm Andre, a software engineer from Los Angeles.
-    //     </p>
-    // </div>
-    
+  return (    
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="home">andrelee.dev</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#https://www.linkedin.com/in/jose-lee-748989183/">
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/jose-lee-748989183/">
               LinkedIn
             </Nav.Link>
-            <Nav.Link href="#https://github.com/joselclee">
+            <Nav.Link href="https://github.com/joselclee">
               Github
             </Nav.Link>
             </Nav>
